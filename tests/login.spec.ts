4adcb8644cfeee917b5e9a10d4a3c085822f77b4
Nikola Ovcahrski AM4 Workstation <nikolaovcharski@gmail.com>
2022-11-11 20:12:52 +0200

    Login test assertions

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,4 +1,4 @@
-import test from "@playwright/test"
+import test, { expect } from "@playwright/test"
 import LoginPage from "../pages/loginPage"
 
 // const username = 'playwrightuser'
@@ -13,6 +13,20 @@ await page.goto('https://ovcharski.com/shop/login/')
 await login.enterUsername('playwrightuser');
 await login.enterPassword('playwrightuser');
 await login.clickLoginBtn();
+
+await expect(page).toHaveTitle('User – Automation Demo Site');
+await expect(page).toHaveURL('https://ovcharski.com/shop/user/playwrightuser/');
+await expect(page.getByRole('heading', { name: 'Automation User'})).toHaveText('Automation User')
+
+// Cover photo:
+
+await expect(page.getByRole('img', { name: 'playwrightuser'})).toBeVisible();
+
+// Profile / avatar photo:
+// The alt text of the cover photo is 'playrwightuser' and the alt text of the profile photo is 'Automations User'. The alt texts are different.
+
+await expect(page.getByAltText('Automation User')).toBeVisible;
+
 
 })
 
