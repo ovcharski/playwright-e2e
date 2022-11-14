6050a0624fc83cd3f07fda7e6e0292d2249b418f
Nikola Ovcahrski Laptop <nikolaovcharski@gmail.com>
2022-11-14 20:49:29 +0200

    fix dublicated code

--- tests/apiExamples.spec.ts
+++ tests/apiExamples.spec.ts
@@ -20,11 +20,3 @@ test('can GET a REST API and check response using assertion style', async ({ req
     expect(body[2].id).toBe(3)
     expect(body[2].title).toBe('Post 3')
 })
-
-test('can POST a REST API and check response using assertion style (using page)', async ({ page }) => {
-    const response = await page.request.post('https://my-json-server.typicode.com/webdriverjsdemo/webdriverjsdemo.github.io/posts', { data: { title: 'Post 4' } })
-    expect(response.status()).toBe(201)
-    const body = JSON.parse(await response.text())
-    expect(body.id).toBe(4)
-    expect(body.title).toBe('Post 4')
-})
