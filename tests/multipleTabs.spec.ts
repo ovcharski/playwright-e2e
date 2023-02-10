121f907c362d26e3ac68819a1a2e762862bd149a
ovcharski <48056945+ovcharski@users.noreply.github.com>
2023-02-10 16:56:41 +0200

    Working with multiple tabs example

---
+++ tests/multipleTabs.spec.ts
@@ -0,0 +1,28 @@
+import { test, expect } from "@playwright/test"
+
+test('Multiple tabs example', async ({ page, context }) => {
+
+// First tab
+    await page.goto('https://en.wikipedia.org/');
+
+// Create second tab
+const newTab = await context.newPage();
+
+// Open page in second tab
+await newTab.goto('https://www.mediawiki.org');
+
+// Bring first tab (Wikipedia) to front
+await page.bringToFront();
+
+// Screenshot first tab
+await page.screenshot({ path: 'screenshots/screenshotWikipedia.png' });
+
+// Screenshot second tab
+await newTab.screenshot({ path: 'screenshots/screenshotMediaWiki.png' });
+
+// Close second tab
+await newTab.close();
+
+
+});
+
