479c5ecc539d5a79e58199138df245fef2f6ea2a
ovcharski <48056945+ovcharski@users.noreply.github.com>
2025-01-14 20:47:47 +0200

    Refactor home and login tests

---
+++ tests/homepage-mobile.spec.ts
@@ -0,0 +1,95 @@
+// tests/homepage.spec.ts
+import { test, expect } from '@playwright/test';
+import HomePage from '../pages/HomePage';
+
+test.describe('Galaxy S10 viewport', () => {
+  test.use({ viewport: { width: 360, height: 760 } });
+  
+  test('Open Homepage', async ({ page }) => {
+    const homePage = new HomePage(page);
+
+    // Go to homepage
+    await page.goto('/shop/');  
+    
+    // Verify text on the homepage
+    await homePage.verifyWelcomeText("Welcome to the store");
+
+    // Capture screenshots
+    await homePage.captureScreenshot('screenshot-s10');
+    await homePage.captureFullPageScreenshot('screenshot-s10');
+    
+    await page.close();
+  });
+});
+
+test.describe('Galaxy S20 viewport', () => {
+  test.use({ viewport: { width: 360, height: 800 } });
+  
+  test('Open Homepage', async ({ page }) => {
+    const homePage = new HomePage(page);
+
+    // Go to homepage
+    await page.goto('/shop/');  
+    
+    // Verify text on the homepage
+    await homePage.verifyWelcomeText("Welcome to the store");
+
+    // Verify additional links visible on the page
+    await expect(page.getByRole('link', { name: ' My Account' })).toBeVisible();
+    await expect(page.getByRole('link', { name: ' Cart' })).toBeVisible();
+
+    // Capture screenshots
+    await homePage.captureScreenshot('screenshot-s20');
+    await homePage.captureFullPageScreenshot('screenshot-s20');
+    
+    await page.close();
+  });
+});
+
+test.describe('Galaxy S20 Ultra viewport', () => {
+  test.use({ viewport: { width: 412, height: 915 } });
+  
+  test('Open Homepage', async ({ page }) => {
+    const homePage = new HomePage(page);
+
+    // Go to homepage
+    await page.goto('/shop/');  
+    
+    // Verify text on the homepage
+    await homePage.verifyWelcomeText("Welcome to the store");
+
+    // Verify additional links visible on the page
+    await expect(page.getByRole('link', { name: ' My Account' })).toBeVisible();
+    await expect(page.getByRole('link', { name: ' Cart' })).toBeVisible();
+
+    // Capture screenshots
+    await homePage.captureScreenshot('screenshot-s20-ultra');
+    await homePage.captureFullPageScreenshot('screenshot-s20-ultra');
+    
+    await page.close();
+  });
+});
+
+test.describe('Galaxy iPhone SE 2 viewport', () => {
+  test.use({ viewport: { width: 375, height: 667 } });
+  
+  test('Open Homepage', async ({ page }) => {
+    const homePage = new HomePage(page);
+
+    // Go to homepage
+    await page.goto('/shop/');  
+    
+    // Verify text on the homepage
+    await homePage.verifyWelcomeText("Welcome to the store");
+
+    // Verify additional links visible on the page
+    await expect(page.getByRole('link', { name: ' My Account' })).toBeVisible();
+    await expect(page.getByRole('link', { name: ' Cart' })).toBeVisible();
+
+    // Capture screenshots
+    await homePage.captureScreenshot('screenshot-SE');
+    await homePage.captureFullPageScreenshot('screenshot-SE');
+    
+    await page.close();
+  });
+});
---
+++ tests/homepage.spec.ts
@@ -0,0 +1,29 @@
+import { test, expect } from '@playwright/test';
+import HomePage from '../pages/HomePage';
+
+test.afterEach(async ({ page }, testInfo) => {
+  console.log(`Finished ${testInfo.title} with status ${testInfo.status}`);
+
+  if (testInfo.status !== testInfo.expectedStatus)
+    console.log(`Did not run as expected, ended up at ${page.url()}`);
+});
+
+test('Open homepage and test few texts @Regression', async ({ page }) => {
+  const homePage = new HomePage(page);
+
+  // Go to homepage
+  await page.goto('/shop/');
+
+  // Verify text on the homepage
+  await homePage.verifyWelcomeText("Welcome to the store");
+
+  // Verify footer content
+  await homePage.verifyFooterText("© Automation Demo Site 2025");
+
+  // Capture screenshots
+  await homePage.captureScreenshot('homepage');
+  await homePage.captureFullPageScreenshot('homepage');
+
+  // Close the page after the test
+  await page.close();
+});
